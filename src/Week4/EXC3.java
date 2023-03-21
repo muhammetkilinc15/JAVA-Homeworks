5f67746a400b3c0c6fec113639382e8e0b57051e
Muhammet Kılınç <mami@DESKTOP-4GP61KD>
2023-03-21 21:13:03 +0300

    Updated

--- src/Week4/EXC3.java
+++ src/Week4/EXC3.java
@@ -2,6 +2,21 @@ package Week4;
 
 public class EXC3 {
     public static void main(String[] args) {
+        System.out.println("Twin primes: ");
+        for (int i=2;i<1200;i++){
+            if (isPrime(i-2) && isPrime(i)){
+                System.out.println("("+(i-2)+", "+(i)+")");
+            }
+        }
+    }
 
+    // This method checks if a number is prime
+    public static boolean isPrime(int number){
+        for (int i=2;i<10;i++){
+            if (number%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/Week4/EXC4.java
+++ src/Week4/EXC4.java
@@ -3,5 +3,22 @@ package Week4;
 public class EXC4 {
     public static void main(String[] args) {
 
+        for (int i = 2; i < 1000; i++) {
+
+            if (isPrime(i) && isPrime(i + 2)) {
+                System.out.printf("(%d, %d)\n", i, i + 2);
+            }
+        }
+    }
+
+    public static boolean isPrime(long n) {
+
+        if (n < 2) return false;
+
+        for (int i = 2; i <= n / 2; i++) {
+
+            if (n % i == 0) return false;
+        }
+        return true;
     }
 }
