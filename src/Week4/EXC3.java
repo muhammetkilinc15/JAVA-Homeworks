9f7f50c1f4dc7ae5b6beab1b9156e99488ac985a
Muhammet Kılınç <mami@DESKTOP-4GP61KD>
2023-03-21 21:17:51 +0300

    Updated

--- src/Week4/EXC3.java
+++ src/Week4/EXC3.java
@@ -1,6 +1,15 @@
 package Week4;
 
 public class EXC3 {
+    /** Twin primes are a pair of prime numbers that differ by 2. For example, 3 and 5 are twin primes, 5
+     and 7 are twin primes, and 11 and 13 are twin primes. Write a program to find all twin primes less
+     than 1,200. Display the output as follows:
+     (3,5)
+     (11,13)
+     (17,19)
+     ...
+
+     */
     public static void main(String[] args) {
         System.out.println("Twin primes: ");
         for (int i=2;i<1200;i++){
@@ -9,9 +18,10 @@ public class EXC3 {
             }
         }
     }
-
     // This method checks if a number is prime
     public static boolean isPrime(int number){
+        if (number<2)
+            return false;
         for (int i=2;i<10;i++){
             if (number%i==0){
                 return false;
