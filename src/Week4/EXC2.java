d79ac5a6988c9442821d86be97deb8ddad458f14
Muhammet Kılınç <mami@DESKTOP-4GP61KD>
2023-03-21 19:17:09 +0300

    Updated

--- src/Week4/EXC2.java
+++ src/Week4/EXC2.java
@@ -3,6 +3,13 @@ package Week4;
 import java.util.Random;
 
 public class EXC2 {
+    /**(Game: craps) Craps is a popular dice game played in casinos. Write a program
+     to play a variation of the game, as follows:
+     Roll two dice. Each die has six faces representing values 1, 2, . . ., and 6, respectively. Check the sum of the two dice.
+     If the sum is 2, 3, or 12 (called craps), you lose; if the sum is 7 or 11 (called natural), you win; if the sum is another value
+     (i.e., 4, 5, 6, 8, 9, or 10), a point is established. Continue to roll the dice until either
+     a 7 or the same point value is rolled. If 7 is rolled, you lose. Otherwise, you win.
+     */
     public static void main(String[] args) {
         int[] diceNumbers = new int[]{1, 2, 3, 4, 5, 6};
         Random random = new Random();
@@ -36,12 +43,14 @@ public class EXC2 {
             }
         }
     }
+    // If total is equal to 2-3-12 and count is equal zero ,return true
     public static boolean loseGame(int total,int count) {
         if (count == 0){
             return total == 2 || total == 3 || total == 12;
         }
         return false;
     }
+    // If total is equal to 7-11 and count is equal zero , return true
     public static boolean winGame(int total, int count) {
         if (count == 0) {
             return total == 7 || total == 11;
